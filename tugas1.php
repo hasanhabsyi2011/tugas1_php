97d46baac98918ee61efcdfd1f39510777b33d5e
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-07 10:16:25 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+
+<?php 
+
+$ayamgoreng = 13000;
+$ayambakar = 15000;
+$ayamsayur = 13000;
+$tempegoreng = 1000;
+$tahugoreng = 1000;
+$soto = 3000;
+$nasiputih = 5000;
+$esteh = 4000;
+$estebu = 5000;
+
+?>
+
+<table>
+			<tr>
+				<td>Ayam Goreng</td>
+				<td>= Rp. <?php echo $ayamgoreng ?></td>
+			</tr>
+			<tr>
+				<td>Ayam Bakar</td>
+				<td>= Rp. <?php echo $ayambakar ?></td>
+			</tr>
+			<tr>
+				<td>Ayam Sayur</td>
+				<td>= Rp. <?php echo $ayamsayur ?></td>
+			</tr>
+			<tr>
+				<td>Tempe Goreng</td>
+				<td>= Rp. <?php echo $tempegoreng ?></td>
+			</tr>
+			<tr>
+				<td>Tahu Goreng</td>
+				<td>= Rp. <?php echo $tahugoreng ?></td>
+			</tr>
+			<tr>
+				<td>Soto</td>
+				<td>= Rp. <?php echo $soto ?></td>
+			</tr>
+			<tr>
+				<td>Nasi Putih</td>
+				<td>= Rp. <?php echo $nasiputih ?></td>
+			</tr>
+			<tr>
+				<td>Es Teh</td>
+				<td>= Rp. <?php echo $esteh ?></td>
+			</tr>
+			<tr>
+				<td>Es Tebu</td>
+				<td>= Rp. <?php echo $estebu ?></td>
+			</tr>
+<table>
+
+ 
+ </body>
+</html>
